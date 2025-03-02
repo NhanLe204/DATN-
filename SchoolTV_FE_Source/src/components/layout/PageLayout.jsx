c85d1b849d8394311396513eac8aece5d877f93f
ngocthanhnt00 <thanhnnps36626@fpt.edu.vn>
2025-03-02 22:36:41 +0700

    feat: tesst

--- SchoolTV_FE_Source/src/components/layout/PageLayout.jsx
+++ SchoolTV_FE_Source/src/components/layout/PageLayout.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import Header from '../Header'
-import { Outlet } from 'react-router-dom'
-import Footer from '../Footer'
+import React from "react";
+import Header from "../header";
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer";
 
 function PageLayout() {
   return (
     <>
-        <Header />
-        <Outlet />
-        <Footer />
+      <p>dsadasdasdasdas</p>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default PageLayout
+export default PageLayout;
