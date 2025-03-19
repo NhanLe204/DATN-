4f47cb8ed00fa39f5ae5dadca7679ed74028ad71
ngocthanhnt00 <thanhnnps36626@fpt.edu.vn>
2025-03-19 15:09:09 +0700

    feat: add method DELETE for Brand

--- backend/dist/routes/brand.routes.js
+++ backend/dist/routes/brand.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { requireAdmin } from '../middlewares/protectRoute.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
-import { getAllBrands, getBrandById, insertBrand, updateBrand } from '../controllers/brand.controllers.js';
+import { deleteBrand, getAllBrands, getBrandById, insertBrand, updateBrand } from '../controllers/brand.controllers.js';
 const brandRouter = Router();
 brandRouter.get('/brands', getAllBrands);
 brandRouter.get('/brands/:id', getBrandById);
 brandRouter.post('/brands', verifyToken, requireAdmin, insertBrand);
 brandRouter.patch('/brands/:id', verifyToken, requireAdmin, updateBrand);
+brandRouter.delete('/brands/:id', verifyToken, requireAdmin, deleteBrand);
 export default brandRouter;
 //# sourceMappingURL=brand.routes.js.map
