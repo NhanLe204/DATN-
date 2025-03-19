d3d9561fbcc23cb791a4c7df74e77e36b30984cf
ngocthanhnt00 <thanhnnps36626@fpt.edu.vn>
2025-03-19 11:36:30 +0700

    feat: BE add method updateTagName

--- backend/dist/routes/brand.routes.js
+++ backend/dist/routes/brand.routes.js
@@ -3,10 +3,9 @@ import { requireAdmin } from '../middlewares/protectRoute.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
 import { getAllBrands, getBrandById, insertBrand, updateBrand } from '../controllers/brand.controllers.js';
 const brandRouter = Router();
-brandRouter.get('/brands', verifyToken, getAllBrands);
-brandRouter.get('/brands/:id', verifyToken, getBrandById);
+brandRouter.get('/brands', getAllBrands);
+brandRouter.get('/brands/:id', getBrandById);
 brandRouter.post('/brands', verifyToken, requireAdmin, insertBrand);
 brandRouter.patch('/brands/:id', verifyToken, requireAdmin, updateBrand);
-// categoryRouter.delete('/categories/:id', protectRoute, requireAdmin, toggleCategory);
 export default brandRouter;
 //# sourceMappingURL=brand.routes.js.map
--- backend/dist/routes/tag.routes.js
+++ backend/dist/routes/tag.routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { requireAdmin } from '../middlewares/protectRoute.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
-import { getAllTags, getTagById, insertTag, deleteTag } from '../controllers/tag.controllers.js';
+import { getAllTags, getTagById, insertTag, deleteTag, updateTag } from '../controllers/tag.controllers.js';
 const tagRouter = Router();
-tagRouter.get('/tags', verifyToken, getAllTags);
-tagRouter.get('/tags/:id', verifyToken, getTagById);
+tagRouter.get('/tags', getAllTags);
+tagRouter.get('/tags/:id', getTagById);
 tagRouter.post('/tags', verifyToken, requireAdmin, insertTag);
 tagRouter.delete('/tags/:id', verifyToken, requireAdmin, deleteTag);
+tagRouter.patch('/tags/:id', verifyToken, requireAdmin, updateTag);
 // categoryRouter.delete('/categories/:id', protectRoute, requireAdmin, toggleCategory);
 // brandRouter.get('/brands/:id', getBrandById);
 // brandRouter.post('/brands', verifyToken, requireAdmin, insertBrand);
