ee1f12ec085464cefe1ac270c58aa4231e79cf16
ngocthanhnt00 <thanhnnps36626@fpt.edu.vn>
2025-03-30 20:14:32 +0700

    feat: BE service router

--- backend/dist/routes/service.routes.js
+++ backend/dist/routes/service.routes.js
@@ -1,17 +1,16 @@
-import { Router } from 'express';
-import { getAllBlogs, createBlog, getBlogById, updateBlog, deleteBlog } from '../controllers/blog.controllers.js';
-import { verifyToken } from '../middlewares/verifyToken.js';
+import { getAllServices, createService, getServiceActive, getServiceById, updateService, deleteService } from '../controllers/service.controllers.js';
 import { requireAdmin } from '../middlewares/protectRoute.js';
-const blogRouter = Router();
-// Lấy tất cả bài viết
-blogRouter.get('/blogs', getAllBlogs);
-// Lấy bài viết theo ID
-blogRouter.get('/blogs/:id', getBlogById);
-// Tạo bài viết mới (yêu cầu xác thực và quyền admin)
-blogRouter.post('/blogs', verifyToken, requireAdmin, createBlog);
-// Cập nhật bài viết theo ID (yêu cầu xác thực và quyền admin)
-blogRouter.patch('/blogs/:id', verifyToken, requireAdmin, updateBlog);
-// Xóa bài viết theo ID (yêu cầu xác thực và quyền admin)
-blogRouter.delete('/blogs/:id', verifyToken, requireAdmin, deleteBlog);
-export default blogRouter;
+import { verifyToken } from '../middlewares/verifyToken.js';
+import { Router } from 'express';
+const serviceRouter = Router();
+// Định nghĩa các route cho Service
+serviceRouter.get('/services', verifyToken, requireAdmin, getAllServices); // Lấy tất cả dịch vụ
+serviceRouter.post('/services', createService); // Tạo mới một dịch vụ
+serviceRouter.get('/services/status/active', getServiceActive);
+serviceRouter.get('/services/:id', getServiceById);
+serviceRouter.patch('/services/:id', verifyToken, requireAdmin, updateService); // Cập nhật dịch vụ theo serviceID
+serviceRouter.delete('/services/:id', verifyToken, requireAdmin, deleteService);
+// serviceRouter.get('/services/:id', getServiceById); // Lấy dịch vụ theo serviceID
+// serviceRouter.delete('/services/:id', deleteService); // Xóa dịch vụ theo serviceID
+export default serviceRouter;
 //# sourceMappingURL=service.routes.js.map
