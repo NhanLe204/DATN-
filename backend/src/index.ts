2498c37accd0b2c6d4ddbe99a57540917ce858b5
NhanLe204 <tranmailenhan204@gmail.com>
2026-01-03 15:52:50 +0700

    fix BE

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -46,9 +46,6 @@ const corsOptions = {
 // };
 
 app.use(cors(corsOptions));
-
-
-app.use(cors(corsOptions));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
